b5ff390a630e08fc0bb93dc6411ac70b835a1f57
Altherlex Alves <para.alves@gmail.com>
2022-01-30 23:25:29 -0800

    use WhiteNoise v5

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
 SILENCED_SYSTEM_CHECKS = [
     "security.W001",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 DATABASES = {
