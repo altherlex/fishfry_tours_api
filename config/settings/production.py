df9666a65053793a7374432fee759273e7b99ec6
Altherlex Alves <para.alves@gmail.com>
2022-01-30 18:18:59 -0800

    use WhiteNoise v5

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ ALLOWED_HOSTS = get_env("DJANGO_ALLOWED_HOSTS").split(",")
 
 STATIC_URL = get_env("DJANGO_STATIC_URL")
 STATIC_ROOT = get_env("DJANGO_STATIC_ROOT")
-MIDDLEWARE = ["whitenoise.django.GzipManifestStaticFilesStorage"] + MIDDLEWARE
+MIDDLEWARE = ["whitenoise.middleware.WhiteNoiseMiddleware"] + MIDDLEWARE
 
 # Nginx is used instead of SecurityMiddleware
 # for setting all the recommended security headers
